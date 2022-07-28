a9b0d04d8345e2fdec548f6a460b3cf2c4006dd1
Chocker926 <104671135+Chocker926@users.noreply.github.com>
2022-07-28 09:31:52 +0800

    Update SM3_Birth_attack.py

--- SM3_Birth_attack/SM3_Birth_attack.py
+++ SM3_Birth_attack/SM3_Birth_attack.py
@@ -10,24 +10,39 @@ def my_sm3(m):
     return m
 
 def birthday_attck(n):
-    for i in range(2**n):
-        b=random.randint(0,2**n)
-        original_b=b
-        my_sm3(b)
-        c=random.randint(0,2**n)
-        original_c=c
-        my_sm3(c)
-        if b ==c:
-            print(original_b,original_c)
+    dict={}
+    for i in range(2**256):
+        b=random.randint(0,2**(16*n))
+        original_b=bytes(hex(b)[2:], encoding="utf8")
+        b=my_sm3(b)
+        dict[b[:n]]=original_b
+        c=random.randint(0,2**(16*n))
+        original_c = bytes(hex(c)[2:], encoding="utf8")
+        c=my_sm3(c)
+        if c[:n] in dict.keys():
+            if dict[c[:n]] == original_c:
+                break
             print("找到碰撞")
+            print(dict[c[:n]])
+            print(original_c)
             print("查找了{0}次".format(i))
             return
+        else:
+            dict[c[:n]]=original_c
     print("失败")
 
 
 
 if __name__=="__main__":
+    start = time.time()
+    birthday_attck(4)
+    end = time.time()
+    print("运行时间为:{0}".format(end - start))
     start=time.time()
-    birthday_attck(512)
-    end=time.time()
-    print("运行时间为:{0}".format(end-start))
+    birthday_attck(8)
+    end = time.time()
+    print("运行时间为:{0}".format(end - start))
+    start = time.time()
+    birthday_attck(16)
+    end = time.time()
+    print("运行时间为:{0}".format(end - start))
