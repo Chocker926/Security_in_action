0db93bc6603b089c2cffdd62c8d64093f4569702
Chocker926 <104671135+Chocker926@users.noreply.github.com>
2022-07-28 11:39:26 +0800

    Update Merkle_Tree.py

--- Merkle Tree/Merkle_Tree.py
+++ Merkle Tree/Merkle_Tree.py
@@ -37,5 +37,5 @@ def concat_and_hash_list(lst, hash_function = 'sha256'):
     return lst1, n+1
 
 
-l = [random.choice(string.ascii_letters) for i in range(2**19) ]
+l = [random.choice(string.ascii_letters) for i in range(2**18) ]
 print(concat_and_hash_list(l))
